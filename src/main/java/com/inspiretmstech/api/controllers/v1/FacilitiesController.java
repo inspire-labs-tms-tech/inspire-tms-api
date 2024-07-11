de0ccb17b28c442527e08a58cf005fe26c8224d6
Nicholas Barrow <nbarrow@inspiretmstech.com>
2024-07-11 16:06:37 -0400

    feat: updating facilities

--- src/main/java/com/inspiretmstech/api/controllers/v1/FacilitiesController.java
+++ src/main/java/com/inspiretmstech/api/controllers/v1/FacilitiesController.java
@@ -12,13 +12,11 @@ import com.inspiretmstech.db.tables.records.FacilitiesRecord;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @Tag(name = "Facilities", description = "User endpoints for managing facilities")
@@ -32,22 +30,46 @@ public class FacilitiesController {
     public IDResponse createFacility(
             @RequestBody FacilityRequest request
     ) throws SQLException {
-        Optional<FacilitiesRecord> facility = DatabaseConnection.getInstance().with(supabase ->
-                supabase
-                        .insertInto(Tables.FACILITIES,
-                                Tables.FACILITIES.DISPLAY,
-                                Tables.FACILITIES.IS_ACTIVE,
-                                Tables.FACILITIES.ADDRESS
-                        )
-                        .values(
-                                request.displayName(),
-                                request.isActive(),
-                                request.address().toAddress()
-                        ).returning().fetchOne());
+        Optional<FacilitiesRecord> facility = DatabaseConnection.getInstance().with(supabase -> supabase
+                .insertInto(Tables.FACILITIES,
+                        Tables.FACILITIES.DISPLAY,
+                        Tables.FACILITIES.IS_ACTIVE,
+                        Tables.FACILITIES.ADDRESS
+                )
+                .values(
+                        request.displayName(),
+                        request.isActive(),
+                        request.address().toAddress()
+                )
+                .returning()
+                .fetchOne());
         if (facility.isEmpty())
-            throw new ResponseException("Facilities could not be created");
+            throw new ResponseException("Facility could not be created", "An unexpected error occurred while creating the facility", "Is the displayName unique?");
 
         return IDResponse.from(facility.get().getId());
     }
+
+    @Secured(Authority.Authorities.USER)
+    @Requires({Scopes.FACILITIES})
+    @Operation(summary = "Update a facility")
+    @PutMapping("{id}")
+    public void createFacility(
+            @RequestBody FacilityRequest request,
+            @PathVariable String id
+    ) throws SQLException {
+
+        Optional<FacilitiesRecord> updated = DatabaseConnection.getInstance().with(supabase -> supabase
+                .update(Tables.FACILITIES)
+                .set(Tables.FACILITIES.DISPLAY, request.displayName())
+                .set(Tables.FACILITIES.IS_ACTIVE, request.isActive())
+                .set(Tables.FACILITIES.ADDRESS, request.address().toAddress())
+                .where(Tables.FACILITIES.ID.eq(UUID.fromString(id)))
+                .returning()
+                .fetchOne());
+        if(updated.isEmpty())
+            throw new ResponseException("Facility could not be updated", "An unknown exception occurred while updating the facility", "Does the facility exist?");
+    }
+
+
 
 }
