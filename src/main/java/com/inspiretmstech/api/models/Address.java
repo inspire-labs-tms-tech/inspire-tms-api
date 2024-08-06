dc1dc609eb13b0796a30b27360f1294ff5ea4ed6
Nicholas Barrow <nbarrow@inspiretmstech.com>
2024-08-06 19:41:32 -0400

    fix: dependency refactoring

--- src/main/java/com/inspiretmstech/api/models/Address.java
+++ src/main/java/com/inspiretmstech/api/models/Address.java
@@ -1,8 +1,8 @@
 package com.inspiretmstech.api.models;
 
 import com.google.maps.model.LatLng;
-import com.inspiretmstech.api.utils.Geocoder;
-import com.inspiretmstech.api.utils.TimeZones;
+import com.inspiretmstech.common.utils.Geocoder;
+import com.inspiretmstech.common.utils.TimeZones;
 import com.inspiretmstech.db.udt.records.AddressRecord;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
@@ -31,7 +31,7 @@ public record Address(
 
     public AddressRecord toAddress() {
         LatLng coords = Geocoder.geocode(this.toString());
-        ZoneId zone = TimeZones.lookup(coords, this);
+        ZoneId zone = TimeZones.lookup(coords, this.toString());
         return new AddressRecord(
                 this.line1,
                 this.line2,
